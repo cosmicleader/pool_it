10f62b7fdea1999f0a309bb3689c4d6a62b6a081
Mehdi Hassan <44486547+cosmicleader@users.noreply.github.com>
2023-05-08 14:14:51 +0100

    Created user page

--- app/app.js
+++ app/app.js
@@ -1,7 +1,9 @@
+
 // Import express.js
 const express = require("express");
 const { render } = require("pug");
 
+const { User } = require("./models/user");
 // Create express app
 var app = express();
 // Use the Pug templating engine
@@ -9,6 +11,15 @@ app.set('view engine', 'pug');
 app.set('views', './app/views');
 // Add static files location
 app.use(express.static("static"));
+// Set the sessions
+var session = require('express-session');
+app.use(session({
+  secret: 'secretkeysdfjsflyoifasd',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}));
+
 
 // Get the functions in the db.js file to use
 const db = require('./services/db');
@@ -17,10 +28,11 @@ const db = require('./services/db');
 app.get("/home", function(req, res) {
     res.render("home");
 });
-
-app.get("/login", function(req, res) {
-    res.render("login");
+// Login
+app.get('/login', function (req, res) {
+    res.render('login');
 });
+
 app.get("/long_trip", function(req, res) {
     res.render("long_trip");
 });
@@ -30,9 +42,9 @@ app.get("/routematching", function(req, res) {
 });
 
 //dynamic route for profile data
-app.get("/profile/:id",function(req ,res){
+app.get("/profile",function(req ,res){
     // res.render('index');
-    var id = req.params.id;
+    var id =  req.session.uid;
     console.log(id);
     sql='SELECT * FROM profile where profile_id = ?';
     db.query(sql,[id]).then(results =>{
@@ -43,7 +55,96 @@ app.get("/profile/:id",function(req ,res){
         
     });
 });
+// Logout
+app.get('/logout', function (req, res) {
+    req.session.destroy();
+    res.redirect('/login');
+  });
 
+app.post('/set-password', async function (req, res) {
+    params = req.body;
+    var user = new User(params.email);
+    try {
+        uId = await user.getIdFromEmail();
+        if (uId) {
+            // If a valid, existing user is found, set the password and redirect to the users single-student page
+            await user.setUserPassword(params.password);
+            res.redirect('/single-student/' + uId);
+        }
+        else {
+            // If no existing user is found, add a new one
+            newId = await user.addUser(params.email);
+            res.send('Perhaps a page where a new user sets a programme would be good here');
+        }
+    } catch (err) {
+        console.error(`Error while adding password `, err.message);
+    }
+});
+// Check submitted email and password pair
+// app.post('/authenticate', async function (req, res) {
+//     params = req.body;
+//     var user = new User(params.email);
+//     try {
+//         uId = await user.getIdFromEmail();
+//         if (uId) {
+//             match = await user.authenticate(params.password);
+//             if (match) {
+//                 res.redirect('/single-student/' + uId);
+//             }
+//             else {
+//                 // TODO improve the user journey here
+//                 res.send('invalid password');
+//             }
+//         }
+//         else {
+//             res.send('invalid email');
+//         }
+//     } catch (err) {
+//         console.error(`Error while comparing `, err.message);
+//     }
+// });
+
+
+
+
+// Check submitted email and password pair
+app.post('/authenticate', async function (req, res) {
+    params = req.body;
+    var user = new User(params.email);
+    try {
+        uId = await user.getIdFromEmail();
+        if (uId) {
+            match = await user.authenticate(params.password);
+            if (match) {
+                req.session.uid = uId;
+                req.session.loggedIn = true;
+                console.log(req.session);
+                res.redirect('/single-student/' + uId);
+            }
+            else {
+                // TODO improve the user journey here
+                res.send('invalid password');
+            }
+        }
+        else {
+            res.send('invalid email');
+        }
+    } catch (err) {
+        console.error(`Error while comparing `, err.message);
+    }
+});
+
+
+// Create a route for root - /
+app.get("/", function(req, res) {
+    console.log(req.session);
+    if (req.session.uid) {
+		res.send('Welcome back, ' + req.session.uid + '!');
+	} else {
+		res.send('Please login to view this page!');
+	}
+	res.end();
+});
 
 
 
