89be72246ded5cf80816ab439a109e570c8c2b38
Mehdi Hassan <44486547+cosmicleader@users.noreply.github.com>
2023-05-08 21:04:19 +0100

    app.js

--- app/app.js
+++ app/app.js
@@ -62,7 +62,7 @@ var id = 5
 //   if(req.session.uid==null){render('login');};
   console.log(id);
   sql = "SELECT * FROM Users where id = ?";
-  db.query(sql, [id]).then((results) => {
+  db.query(sql, [user.id]).then((results) => {
     console.log(results);
     //res.send(results)
     res.render("profile", {
